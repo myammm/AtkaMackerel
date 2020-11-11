6d161fde96a0e949a18e4c00bca713ac8e32f41b
myammm <dmy.choco@me.com>
2020-11-11 19:57:44 +0900

    initial commit.

---
+++ AtkaMackerel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AtkaMackerel
+//
+//  Created by Yamada Maya on 2020/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
